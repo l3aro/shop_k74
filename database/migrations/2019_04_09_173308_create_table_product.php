1c2e078e7991e00e1fc15fc506e5101dac6ade60
l3lackheart <dgbao1340@gmail.com>
2019-04-10 18:57:34 +0700

    Init migration

---
+++ database/migrations/2019_04_09_173308_create_table_product.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->decimal('price', 24, 0)->nullable()->default(0);
+            $table->boolean('highlight')->nullable()->default(false);
+            $table->integer('quantity')->unsigned()->nullable()->default(0);
+            $table->string('avatar')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- database/migrations/2019_04_09_173528_create_table_order.php
+++ database/migrations/2019_04_09_173528_create_table_order.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTableOrder extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('fullname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('address')->nullable();
+            $table->string('client_name')->nullable();
             $table->string('phone', 24)->nullable();
-            $table->string('level', 10)->nullable();
-            $table->rememberToken();
+            $table->string('address')->nullable();
+            $table->string('email')->nullable();
+            $table->string('status', 10)->nullable();
             $table->timestamps();
         });
     }
@@ -34,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 }
