e1b1e9928f5b4ec529a97285fb0d23d3d1c219e4
l3lackheart <dgbao1340@gmail.com>
2019-04-12 18:53:35 +0700

    Match action

---
+++ app/Http/Controllers/Client/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function detail(Product $product) {
+        
+        return $product->name;
+    }
+
+    public function shop() {
+        return 'Danh sách';
+    }
+}
