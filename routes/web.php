b766ada6b24c93a28483d102f439902a20198158
l3lackheart <dgbao1340@gmail.com>
2019-04-12 18:53:29 +0700

    Init route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,69 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/**
+ * Client Zone
+ */
+Route::group([
+    'namespace' => 'Client'
+], function() {
+    Route::get('/', 'HomeController@index');
+    Route::get('gioi-thieu', 'HomeController@about');
+    Route::get('lien-he', 'HomeController@contact');
+
+    Route::get('gio-hang', 'CartController@index');
+    Route::get('gio-hang/thanh-toan', 'CartController@checkout');
+    Route::get('gio-hang/thanh-cong', 'CartController@complete');
+
+    Route::get('san-pham/{product}', 'ProductController@detail');
+    Route::get('san-pham', 'ProductController@shop');
+        
+});
+ 
+/**
+ * Admin Zone
+ */
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+
+    Route::get('login', 'LoginController@showLoginForm');
+
+    Route::get('/', 'DashboardController@index');
+
+    // Admin Product routes
+    Route::group([
+        'prefix' => 'products'
+    ], function() {
+        Route::get('/', 'ProductController@index');
+        Route::get('create', 'ProductController@create');
+        Route::get('{product}/edit', 'ProductController@edit');
+    });
+
+    // Admin User routes
+    Route::group([
+        'prefix' => 'users'
+    ], function () {
+        Route::get('create', 'UserController@create');
+        Route::get('{user}/edit', 'UserController@edit');
+        Route::get('/', 'UserController@index');
+    });
+    
+    // Admin Category routes
+    Route::group([
+        'prefix' => 'categories'
+    ], function () {
+        Route::get('/', 'CategoryController@index');
+        Route::get('{category}/edit', 'CategoryController@edit');
+    });
+
+    // Admin Order routes
+    Route::group([
+        'prefix' => 'orders'
+    ], function () {
+        Route::get('{order}/edit', 'OrderController@edit');
+        Route::get('/', 'OrderController@index');
+        Route::get('processed', 'OrderController@processed');
+    });    
 });
